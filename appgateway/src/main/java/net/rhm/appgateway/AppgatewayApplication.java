0bdd3522f3f93d9aed46824ae17c2de5c56923e7
eddymio <contact@yasi.fr>
2019-01-20 21:35:41 +0100

    updating oauth2 details and adding dependencies within gateway so it can work with JDK 11.

--- appgateway/src/main/java/net/rhm/appgateway/AppgatewayApplication.java
+++ appgateway/src/main/java/net/rhm/appgateway/AppgatewayApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableOAuth2Sso
 @EnableEurekaClient
 @EnableZuulProxy
+@EnableSwagger2
 public class AppgatewayApplication {
 
     public static void main(String[] args) {
