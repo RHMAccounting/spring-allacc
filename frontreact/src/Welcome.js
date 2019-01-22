c9efa9b64e55510a24c93cfa4509718b0ce6ef52
eddymio <contact@yasi.fr>
2019-01-22 22:54:43 +0100

    adding missing files to git.

---
+++ frontreact/src/Welcome.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+    render() {
+        return (
+            <div className="welcome">
+                <p>Welcome here ! Click the following link to sign in...</p>
+            </div>
+        );
+    }
+}
+export default Welcome;
