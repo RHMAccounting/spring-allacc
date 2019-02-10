86ec9578b09fe7682dbf314f8d728a48d216cae5
eddymio <contact@yasi.fr>
2019-02-10 17:52:04 +0100

    Preparing react app with home page and login button linking to Gateway.

--- frontreact/src/App.js
+++ frontreact/src/App.js
@@ -1,29 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import logo from './rhm.png';
-import Welcome from './Welcome';
-import Secured from './Secured';
+import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <CookiesProvider>
+          <Router>
+            <Switch>
+              <Route path='/' exact={true} component={Home}/>
 
-          <BrowserRouter>
-            <div className="container">
-              <ul>
-                <li><Link to="/">public component</Link></li>
-                <li><Link to="/secured">secured component</Link></li>
-              </ul>
-              <Route exact path="/" component={Welcome} />
-              <Route path="/secured" component={Secured} />
-            </div>
-          </BrowserRouter>
-        </header>
-      </div>
+            </Switch>
+          </Router>
+        </CookiesProvider>
     );
   }
 }
