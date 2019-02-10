93f5a97ea47aa1b740b05167faeb841a70ede207
eddymio <contact@yasi.fr>
2019-02-10 17:36:43 +0100

    Updating Keycloak configuration within config files - About to remove OAuth2 implementation from within frontend part.

--- frontreact/src/QueryAPI.js
+++ frontreact/src/QueryAPI.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import APIResponse from "./APIResponse";
+// Import the Log class...
+import Log from './Log';
+
 
 class QueryAPI extends Component {
 
@@ -9,29 +12,49 @@ class QueryAPI extends Component {
     }
 
     authorizationHeader() {
-        if(!this.props.keycloak) return {};
-        return {
-            headers: {
-                "Authorization": "Bearer " + this.props.keycloak.token
-            }
-        };
+        if(!this.props.keycloak) {
+            Log.info('authorization header !', 'missing keycloak props');
+            return {};
+        } else {
+
+            const myHeaders = new Headers();
+
+            //myHeaders.set("Content-Type", 'application/json');
+            myHeaders.set("Authorization", "Bearer " + this.props.keycloak.token);
+
+            Log.info('authorization header !', 'sending token keycloak props ' + this.props.keycloak.token);
+            return myHeaders;
+
+        }
     }
 
     handleClick = () => {
-        fetch('http://localhost:89/', this.authorizationHeader())
+
+        Log.info('Clicking... ', "just clicked...");
+
+        const myInit = { method: 'GET',
+            headers : this.authorizationHeader(),
+            cache: 'default'};
+
+        const myReq = new Request('http://localhost:89/customer', myInit);
+
+        fetch(myReq, myInit)
             .then(response => {
                 if (response.status === 200)
                     return response.json();
-                else
-                    return { status: response.status, message: response.statusText }
+                else {
+                    Log.debug("else status not 200", "message : " + response.statusText);
+                    return {status: response.status, message: response.statusText}
+                }
             })
             .then(json => this.setState((state, props) => ({
                 response: JSON.stringify(json, null, 2)
             })))
             .catch(err => {
+                Log.error("shit ! " + err.toString())
                 this.setState((state, props) => ({ response: err.toString() }))
             })
-    }
+    };
 
     render() {
         return (
