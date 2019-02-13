e97e2f55d547578120a1bcf68c4ff5aa038a793a
eddymio <contact@yasi.fr>
2019-02-13 22:07:35 +0100

    Gateway successfully leading to user app. - Should fix the trouble for first time use after sending token...

--- appgateway/src/main/java/net/rhm/appgateway/AppgatewayApplication.java
+++ appgateway/src/main/java/net/rhm/appgateway/AppgatewayApplication.java
@@ -3,20 +3,43 @@ package net.rhm.appgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
 @EnableSwagger2
 @EnableOAuth2Sso
+@EnableCircuitBreaker
 public class AppgatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppgatewayApplication.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 }
 
--- appuser/src/main/java/net/rhm/appuser/controller/UserController.java
+++ appuser/src/main/java/net/rhm/appuser/controller/UserController.java
@@ -1,5 +1,7 @@
 package net.rhm.appuser.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +13,16 @@ import java.util.Map;
 @RestController
 public class UserController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/users")
     @SuppressWarnings("unchecked")
     public String howdy(Model model, Principal principal) {
         OAuth2Authentication authentication = (OAuth2Authentication) principal;
         Map<String, Object> user = (Map<String, Object>) authentication.getUserAuthentication().getDetails();
         model.addAttribute("user", user);
+
+        LOGGER.debug("Authenticating user... " + user.toString());
         return "home";
     }
 }
